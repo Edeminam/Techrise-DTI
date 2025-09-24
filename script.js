ad4f74df00ca1d3c8667518c3e0de63321df7764
edeminam <elebrendan@gmail.com>
2025-09-24 04:35:13 +0100

    techrise

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
 "use strict";
 
 // Mobile Hamburger Menu
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".navlinks");
-
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
-
-// Smooth Scroll
-document.querySelectorAll('a[href^="#"]').forEach((link) => {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute("href"));
-    if (target) {
-      window.scrollTo({
-        top: target.offsetTop - 70,
-        behavior: "smooth",
-      });
-      navLinks.classList.remove("active");
-    }
-  });
-});
 
 // Newsletter Form Validation
 const sendBtn = document.querySelector(".send-btn a");
